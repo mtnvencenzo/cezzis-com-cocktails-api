b93e0aef4c78f9ed279679568c5aa3c7477cd43e
Ronaldo Vecchi <rvecchi@gmail.com>
2025-07-05 17:29:35 -0600

    feat (apim docs): Adding apim docs back in

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -32,4 +32,15 @@ locals {
         <on-error><base /></on-error>
       </policies>
     XML
+
+  apim_anonomous_docs_operation_policy = <<XML
+      <policies>
+        <inbound>
+          <set-backend-service backend-id="${var.environment}-${var.domain}-api-backend" />
+        </inbound>
+        <backend><base /></backend>
+        <outbound><base /></outbound>
+        <on-error><base /></on-error>
+      </policies>
+    XML
 }
