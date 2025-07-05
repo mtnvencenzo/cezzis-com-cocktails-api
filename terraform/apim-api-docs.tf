470ff7aeecf6db5f8f0124bb24e3561723b989f7
Ronaldo Vecchi <rvecchi@gmail.com>
2025-07-05 17:33:40 -0600

    feat (apim docs): Adding apim docs back in (#6)

--- terraform/apim-api-docs.tf
+++ terraform/apim-api-docs.tf
@@ -1,41 +1,41 @@
-# module "apim_cocktails_api_docs" {
-#   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/apim-simple-api"
-#   providers = {
-#     azurerm = azurerm
-#   }
+module "apim_cocktails_api_docs" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/apim-api"
+  providers = {
+    azurerm = azurerm
+  }
 
-#   tags = local.tags
+  tags                           = local.tags
+  environment                    = var.environment
+  domain                         = var.domain
+  name_discriminator             = "api-docs"
+  keyvault_apimhostkey_secret_id = azurerm_key_vault_secret.cocktails_api_apimhostkey.id
+  backend_url_override           = "https://${module.aca_cocktails_api.ingress_fqdn}"
 
-#   environment        = var.environment
-#   domain             = var.domain
-#   name_discriminator = "api-docs"
+  apim_instance = {
+    id                  = data.azurerm_api_management.apim_shared.id
+    name                = data.azurerm_api_management.apim_shared.name
+    resource_group_name = data.azurerm_api_management.apim_shared.resource_group_name
+  }
 
-#   apim_instance = {
-#     id                  = data.azurerm_api_management.apim_shared.id
-#     name                = data.azurerm_api_management.apim_shared.name
-#     resource_group_name = data.azurerm_api_management.apim_shared.resource_group_name
-#     principal_id        = data.azurerm_api_management.apim_shared.identity[0].principal_id
-#   }
+  api = {
+    version      = 1
+    service_fqdn = module.aca_cocktails_api.ingress_fqdn
+    ingress_fqdn = module.aca_cocktails_api.ingress_fqdn
+  }
 
-#   api = {
-#     version      = 1
-#     service_fqdn = module.aca_cocktails_api.ingress_fqdn
-#     ingress_fqdn = module.aca_cocktails_api.ingress_fqdn
-#   }
+  application_insights = {
+    id                  = data.azurerm_application_insights.appi.id
+    instrumentation_key = data.azurerm_application_insights.appi.instrumentation_key
+  }
 
-#   application_insights = {
-#     id                  = data.azurerm_application_insights.appi.id
-#     instrumentation_key = data.azurerm_application_insights.appi.instrumentation_key
-#   }
-
-#   operations = [
-#     {
-#       display_name        = "Get Scalar Docs v1"
-#       method              = "GET"
-#       url_template        = "/scalar/*"
-#       description         = "Get Scalar Docs v1"
-#       success_status_code = 200
-#       security_type       = "anonymous"
-#     }
-#   ]
-# }
+  operations = [
+    {
+      display_name        = "Get Scalar Docs v1"
+      method              = "GET"
+      url_template        = "/scalar/*"
+      description         = "Get Scalar Docs v1"
+      success_status_code = 200
+      policy_xml_content  = local.apim_anonomous_docs_operation_policy
+    }
+  ]
+}
