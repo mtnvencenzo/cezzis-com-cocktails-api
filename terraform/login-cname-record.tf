784c0ddb1636b7214e3e94c75885251a8877fc7e
mtnvencenzo <rvecchi@gmail.com>
2025-09-30 07:29:23 -0600

    updates for verification values

--- terraform/login-cname-record.tf
+++ terraform/login-cname-record.tf
@@ -1,10 +1,11 @@
 module "login_cname_record" {
   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/dns-sub-domain-record"
 
-  dns_zone = data.azurerm_dns_zone.cezzis_dns_zone.name
-  ttl      = 300
-  name     = var.auth0_custom_domain_subdomain
-  value    = var.auth0_custom_domain_cname
+  dns_zone                      = data.azurerm_dns_zone.cezzis_dns_zone.name
+  ttl                           = 300
+  sub_domain                    = var.auth0_custom_domain_subdomain
+  record_fqdn                   = var.auth0_custom_domain_cname
+  custom_domain_verification_id = "random-string" # Not used for CNAME but required by module interface
 
   tags = local.tags
 }
