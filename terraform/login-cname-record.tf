fc68478e8c236cf1cd77c1ba4c8adddc785a385b
mtnvencenzo <rvecchi@gmail.com>
2025-09-30 07:51:18 -0600

    fixes

--- terraform/login-cname-record.tf
+++ terraform/login-cname-record.tf
@@ -1,13 +1,11 @@
 module "login_cname_record" {
   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/dns-sub-domain-record"
 
-  dns_zone {
+  dns_zone = {
     resource_group_name = data.azurerm_dns_zone.cezzis_dns_zone.resource_group_name
     name                = data.azurerm_dns_zone.cezzis_dns_zone.name
   }
 
-
-  dns_zone                      = data.azurerm_dns_zone.cezzis_dns_zone.name
   ttl                           = 300
   sub_domain                    = var.auth0_custom_domain_subdomain
   record_fqdn                   = var.auth0_custom_domain_cname
