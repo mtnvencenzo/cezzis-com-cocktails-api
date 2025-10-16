8169b4a44d7a72629d5f596cf7e4a132993cf47c
mtnvencenzo <rvecchi@gmail.com>
2025-10-16 07:30:29 -0600

    fix for bad data ref

--- terraform/ai-search.tf
+++ terraform/ai-search.tf
@@ -3,7 +3,7 @@ module "ai_search_cosmos_index" {
   ai_search_service_host_name = local.ai_search_service_host_name
   ai_search_service_id        = data.azurerm_search_service.ai_search_service.id
   ai_search_service_key       = data.azurerm_search_service.ai_search_service.primary_key
-  cosmosdb_account_id         = data.azurerm_cosmosdb_account.cocktails_cosmosdb_account.id
+  cosmosdb_account_id         = data.azurerm_cosmosdb_account.cosmosdb_account.id
   cosmos_database_name        = var.cocktails_cosmosdb_database_name
   cosmos_container_name       = "cezzis-${var.environment}-cocktails-cocktail"
   tags                        = local.tags
