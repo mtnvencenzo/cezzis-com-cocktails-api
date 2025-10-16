21af1cbd9dc38ad0ec8c7ba7fc2ee56bd7410d8e
mtnvencenzo <rvecchi@gmail.com>
2025-10-16 07:11:49 -0600

    Updates for contain and db vars

--- terraform/ai-search.tf
+++ terraform/ai-search.tf
@@ -4,8 +4,8 @@ module "ai_search_cosmos_index" {
   ai_search_service_id        = data.azurerm_search_service.ai_search_service.id
   ai_search_service_key       = data.azurerm_search_service.ai_search_service.primary_key
   cosmosdb_account_id         = module.cocktails_cosmosdb_account.cosmosdb_account_id
-  cosmos_database_name        = module.cocktails_cosmosdb_database.database_name
-  cosmos_container_name       = "cocktails-cocktail"
+  cosmos_database_name        = var.cocktails_cosmosdb_database_name
+  cosmos_container_name       = "cezzis-${var.environment}-cocktails-cocktail"
   tags                        = local.tags
 
   search_index_reader_role_assignment = [module.aca_cocktails_api.managed_identity_principal_id]
